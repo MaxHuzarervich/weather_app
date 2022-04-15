7180ed7427d888e84efbb347b6012daa9e505659
MaxHuzarevich <pinsk.maks@mail.ru>
2022-04-15 19:10:06 +0300

    Updates

---
+++ precache-manifest.12fa598983a8920eca3748cf1d40fc7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f40804747d9d2a69e6",
+    "url": "/weather_app/static/css/main.8e36fbb1.chunk.css"
+  },
+  {
+    "revision": "c8f40804747d9d2a69e6",
+    "url": "/weather_app/static/js/main.043261dd.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  },
+  {
+    "revision": "505903af35bd7e691d0f",
+    "url": "/weather_app/static/js/2.2db4cc88.chunk.js"
+  },
+  {
+    "revision": "43977edb1781490dcb55c22ccb319c5c",
+    "url": "/weather_app/static/media/field-533541_1280.43977edb.jpg"
+  },
+  {
+    "revision": "12f2ee4d837d194357d3152d9f98deb8",
+    "url": "/weather_app/static/media/sunset-1373171_1920.12f2ee4d.jpg"
+  },
+  {
+    "revision": "ad34399b6e9f3818d9627bdb29d53832",
+    "url": "/weather_app/static/media/sea-164989_1280.ad34399b.jpg"
+  },
+  {
+    "revision": "39ef3930cfe1ab4e100c9e612a61d49d",
+    "url": "/weather_app/static/media/sunrise-1014712_1280.39ef3930.jpg"
+  },
+  {
+    "revision": "d231c1dbc9c27c36004ba3e8c0ba574f",
+    "url": "/weather_app/static/media/tree-736885_1280.d231c1db.jpg"
+  },
+  {
+    "revision": "7e01671788229779c9f8ed4945827c03",
+    "url": "/weather_app/static/media/mountains-190055_1280.7e016717.jpg"
+  },
+  {
+    "revision": "d5db0f710e27a18b778dd2729cb374e9",
+    "url": "/weather_app/index.html"
+  }
+];
