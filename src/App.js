2383f7336230791557d4aa004ad6c9a93053451d
MaxHuzarevich <pinsk.maks@mail.ru>
2022-04-15 19:11:31 +0300

    - add pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=391fdbfd3a3e77266f3d6ffc2d6c43ca`;
+
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data)
+            })
+            setLocation('')
+        }
+    }
+
+    return (
+        <div className='app'>
+            <div className='search'>
+                <input
+                    value={location}
+                    onChange={event => setLocation(event.target.value)}
+                    onKeyPress={searchLocation}
+                    placeholder='Enter location'
+                    type='text'/>
+            </div>
+            <div className='container'>
+                <div className='top'>
+                    <div className='location'>
+                        <p>{data.name}</p>
+                    </div>
+                    <div className='temp'>
+                        {data.main ? <h1>{(((data.main.temp) - 32) * 5/9).toFixed()}°C</h1> : null}
+                    </div>
+                    <div className='description'>
+                        {data.weather ? <p>{data.weather[0].main}</p> : null}
+                    </div>
+                </div>
+                {data.name !== undefined &&
+                <div className='bottom'>
+                    <div className='feels'>
+                        {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}</p> : null}
+                        <p>Feels Like</p>
+                    </div>
+                    <div className='humidity'>
+                        {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                        <p>Humidity</p>
+                    </div>
+                    <div className='wind'>
+                        {data.wind ? <p className='bold'>{(data.wind.speed * 1.60934).toFixed()}km/h</p> : null}
+                        <p>Wind speed</p>
+                    </div>
+                </div>}
+            </div>
+        </div>
+    );
 }
 
-export default App;
